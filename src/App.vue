13026e31033ae0f094308bc36792f1b5a16e9871
Anakin787 <seonbi1998@naver.com>
2022-12-23 18:47:00 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,57 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <v-main>
+      <template>
+        <v-container>
+          <v-data-table
+            :headers="header"
+            :items="data"
+            :items-per-page="5"
+            item-key="id"
+            disable-sort
+            hide-default-footer
+            class="elevation-1"
+          ></v-data-table>
+        </v-container>
+      </template>
+    </v-main>
 
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      key:
+        "bx5Feaub8FfLGLBgQkewGgIrdf5hJaqr%2FGqudrfyCZ7lwc%2FkzoAozvFaT5GXf0CvycvgvbaJC5dgpRS1m%2Fdzog%3D%3D",
+      data: [],
+      header: [{ text: "region_cd", value: "region_cd" }]
+    };
+  },
+  created() {
+    this.testGet();
+  },
+  methods: {
+    testGet() {
+      axios
+        .get(
+          `https://apis.data.go.kr/1741000/StanReginCd/getStanReginCdList?serviceKey=${this.key}&pageNo=1&numOfRows=3&type=json&locatadd_nm=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C`
+        )
+        .then(
+          a => (
+            (this.data = a.data.StanReginCd.row),
+            console.log(a.data.StanReginCd.row[0])
+          )
+        );
+    }
+  }
 };
 </script>
